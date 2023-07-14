2d12a2fa3001558580145cdbae866b5b4988539f
Rafael Grando <sta_rafinha@hotmail.com>
2023-07-14 13:13:29 -0300

    "Projeto criado"

---
+++ src/main/java/com/devsuperior/dslist/MydslistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydslistBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydslistBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devsuperior/dslist/MydslistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydslistBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
