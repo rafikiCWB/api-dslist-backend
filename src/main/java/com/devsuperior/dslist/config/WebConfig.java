dc4eb416893b7c7510fde64105e1e51529353f98
Rafael Grando <sta_rafinha@hotmail.com>
2023-07-19 17:36:46 -0300

    "Add Doc a classe WebConfig"

--- src/main/java/com/devsuperior/dslist/config/WebConfig.java
+++ src/main/java/com/devsuperior/dslist/config/WebConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configuration class for handling CORS (Cross-Origin Resource Sharing) configuration in the web application.
+ */
+
 @Configuration
 public class WebConfig {
 
